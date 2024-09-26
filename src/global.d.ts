19e5bd2b1c116d7188b247f2012290818cfc9dee
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-09-25 21:04:55 -0300

    updates

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 declare module "*.css";
 
-declare module 'ion-icon' {
-    const IonIcon: any;
-    export default IonIcon;
-}
+declare namespace JSX {
+    interface IntrinsicElements {
+      'ion-icon': any; // ou você pode definir um tipo mais específico se souber
+    }
+  }
