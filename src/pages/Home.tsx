c40d856c23d01fbcafd484eb592cc1a5e1796edb
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-11-10 18:32:48 -0300

    feat: Add botão de download do curriculo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,6 +19,22 @@ const Home: React.FC<HomeProps> = ({ isDarkMode }) => {
       <div className="flex justify-center mt-8 mb-4">
         <img src="/joaopedroplinta.jpg" alt="João Pedro Plinta" className="rounded-full w-48 h-48 object-cover shadow-lg"/>
       </div>
+
+      <div className="mt-6">
+        <p className={`text-lg ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>
+          Se você deseja saber mais sobre minha trajetória profissional e habilidades, pode baixar meu currículo clicando no botão abaixo.
+        </p>
+      </div>
+
+      <div className="mt-6">
+      <a
+        href="/curriculo_joao_plinta.pdf" 
+        download="Curriculo_Joao_Pedro_Plinta.pdf"
+        className={`inline-block text-lg ${isDarkMode ? 'text-white' : 'text-gray-800'} px-6 py-3 bg-purple-500 rounded-lg hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300`}
+          >
+          Baixar Currículo
+        </a>
+      </div>
     </main>
   );
 };
